a3216681c1d4a77d245f184750cc67e9c6a2511f
Frank S. Thomas <frank@timepit.eu>
2016-02-03 16:00:35 +0100

    Use latest fshell

--- build.sbt
+++ build.sbt
@@ -3,6 +3,6 @@ enablePlugins(MdocPlugin)
 name := "rendering-engines"
 
 libraryDependencies ++= Seq(
-  "org.m-doc" %% "fshell" % "0.0.0-26-gda81da6",
+  "org.m-doc" %% "fshell" % Version.fshell,
   "org.scalacheck" %% "scalacheck" % Version.scalacheck % "test"
 )
--- src/test/scala/org/mdoc/renderingengines/LibreOfficeSpec.scala
+++ src/test/scala/org/mdoc/renderingengines/LibreOfficeSpec.scala
@@ -13,10 +13,11 @@ class LibreOfficeSpec extends Properties("LibreOffice") {
     val odt = Paths.get("./src/test/resources/test.odt")
     val pdf = Paths.get("./test.pdf")
     val p = for {
+      notExists <- Shell.fileNotExists(pdf)
       _ <- convertToPdf(odt)
       bytes <- Shell.readAllBytes(pdf)
       _ <- Shell.delete(pdf)
-    } yield bytes.size > 1024
+    } yield notExists && bytes.size > 1024
     p.yolo
   }
 }
